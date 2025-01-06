71849ba0c705c392486f24774715114d26ab385f
Rahat Bin Israil <rahat.bin.israil@gmail.com>
2025-01-06 09:55:29 +0600

    Dynamic Content to JSX

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+function DynamicImage() {
+    const imageUrl = "https://via.placeholder.com/150"; // Dynamic Image URL
+    const imageAlt = "Placeholder Image"; // Dynamic alt text
 
+    return (
+        <div>
+            <img src={imageUrl} alt={imageAlt} />
+            <p>{imageAlt}</p>
+        </div>
+    );
+}
+
+export default DynamicImage;
