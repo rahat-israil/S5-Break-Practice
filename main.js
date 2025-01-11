2062c62e375f986d40ee0dd814c3ddf9f27de52e
Rahat Bin Israil <rahat.bin.israil@gmail.com>
2025-01-11 13:00:09 +0600

    Arrow Function

--- main.js
+++ main.js
@@ -59,3 +59,21 @@ const age = 18;
 const eligibility = age >= 18 ? "Eligible to vote" : "Not eligible to vote";
 
 console.log(eligibility); // Output: Eligible to vote
+
+
+
+
+
+const getFiftyFive = () => 55;
+const addSixtyFive = num => num + 65;
+const isEven = x => x % 2 == 0;
+const addThree = (x, y, z) => x + y + z;
+
+const doMath = (num1, num2) => {
+    const sum = num1 + num2;
+    return sum;
+}
+
+
+
+
